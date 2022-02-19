88a521939cc52331d288a7191c5fb31dffe90c72
Nasrullah Khan <nasrullah.khan1047@gmail.com>
2022-02-19 11:15:04 +0500

    Adding Project Files

---
+++ src/store/reducers/user_reducer.js
@@ -0,0 +1,14 @@
+import { ADD_NEWSLETTER, CLEAR_NEWSLETTER, SEND_MESSAGE } from '../types';
+
+export default function userReducer(state = {}, action) {
+  switch (action.type) {
+    case ADD_NEWSLETTER:
+      return { ...state, ...action.payload };
+    case CLEAR_NEWSLETTER:
+      return { ...state, newsletter: action.payload, email: action.payload };
+    case SEND_MESSAGE:
+      return { ...state, ...action.payload };
+    default:
+      return state;
+  }
+}
